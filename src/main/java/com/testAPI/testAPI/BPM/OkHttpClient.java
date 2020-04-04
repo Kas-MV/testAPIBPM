6e2ed1cac08963b964848c2bba4cbc546e946042
Marat Kasimov <kasimov.mv@mail.ru>
2020-04-04 17:16:20 +0400

    Создание потоков

---
+++ src/main/java/com/testAPI/testAPI/BPM/OkHttpClient.java
@@ -0,0 +1,9 @@
+package com.testAPI.testAPI.BPM;
+
+import java.nio.file.attribute.AclEntry;
+
+public class OkHttpClient {
+    public AclEntry.Builder newBuilder() {
+        return null;
+    }
+}
