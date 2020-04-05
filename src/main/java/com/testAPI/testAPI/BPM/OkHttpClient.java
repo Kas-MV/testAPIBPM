eead0078e46d0a90a1228cdf4e58929e6c5c24fa
Marat Kasimov <kasimov.mv@mail.ru>
2020-04-05 21:29:27 +0400

    connect JSON

--- src/main/java/com/testAPI/testAPI/BPM/OkHttpClient.java
+++
@@ -1,9 +0,0 @@
-package com.testAPI.testAPI.BPM;
-
-import java.nio.file.attribute.AclEntry;
-
-public class OkHttpClient {
-    public AclEntry.Builder newBuilder() {
-        return null;
-    }
-}
--- src/main/java/com/testAPI/testAPI/BPM/testAPI.java
+++ src/main/java/com/testAPI/testAPI/BPM/testAPI.java
@@ -1,29 +1,46 @@
 package com.testAPI.testAPI.BPM;
 
+import okhttp3.*;
+
+import java.io.IOException;
+
 class RunnableDemo implements Runnable {
     private Thread t;
     private String threadName;
+
     RunnableDemo(String name) {
         threadName = name;
-        System.out.println("Creating " + threadName);
+//        System.out.println("Creating " + threadName);
     }
 
     public void run() {
         System.out.println("Running " + threadName);
         try {
-            for (int i = 4; i > 0; i++) {
+            for (int i = 1; i < 3; i++) {
+                OkHttpClient client = new OkHttpClient().newBuilder()
+                        .build();
+                MediaType mediaType = MediaType.parse("application/json");
+                RequestBody body = RequestBody.create(mediaType, "{\r\n  \"context\": {\r\n    \"__name\": \"postman " + threadName + " " + i + "\"\r\n  }\r\n}");
+                Request request = new Request.Builder()
+                        .url("https://yelgesunw3swu.s-quickbpm.ru/pub/v1/app/workspace/testbpmapi/create")
+                        .method("POST", body)
+                        .addHeader("X-Token", "3152dc2b-390d-4907-a5c9-e5e9e52bf152")
+                        .addHeader("Content-Type", "application/json")
+                        .build();
+                Response response = client.newCall(request).execute();
+
                 System.out.println("Thread: " + threadName + ", " + i);
                 // Let the thread sleep for a while.
                 Thread.sleep(50);
-                if (i>=1000){
-
-                }
             }
         } catch (InterruptedException e) {
             System.out.println("Thread " + threadName + " interrupted.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         System.out.println("Thread " + threadName + " exiting.");
     }
+
     public void start() {
         System.out.println("Starting " + threadName);
         if (t == null) {
